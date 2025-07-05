e9febab5d24b6fabb22bda4e0d81e3bad8ae2c35
Dean Ellis <dellis1972@googlemail.com>
2025-07-05 14:11:08 +0100

    Slow down the clouds

--- Source/Entities/Cloud.cs
+++ Source/Entities/Cloud.cs
@@ -8,10 +8,11 @@ using Microsoft.Xna.Framework.Graphics;
 
 public class Cloud : BobingEntity
 {
-    
+
     public Cloud(Model model, ContentManager contentManager) : base(model, contentManager)
     {
         SpecularIntensity = 0.1f;
         Shininess = 0.5f;
+        BobSpeed = 1f;
     }
 }
