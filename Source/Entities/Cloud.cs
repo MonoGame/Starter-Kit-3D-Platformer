a676bcb1a465b83f3be2fff7b71127c350c9558f
Dean Ellis <dellis1972@googlemail.com>
2025-07-13 13:52:43 +0100

    We should not be able to stand on clouds

--- Source/Entities/Cloud.cs
+++ Source/Entities/Cloud.cs
@@ -3,17 +3,18 @@
 // file 'LICENSE.md', which is part of this source code package.
 
 using System;
+using System.Text.Json;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
 public class Cloud : BobingEntity
 {
-
     public Cloud(Model model, ContentManager contentManager) : base(model, contentManager)
     {
         SpecularIntensity = 0.1f;
         Shininess = 0.5f;
         BobSpeed = 1f;
+        IsBlockingMovement = false; // Spawn points should not block movement
     }
 }
