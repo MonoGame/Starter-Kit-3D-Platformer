ca8c17c3e9a85148e164b94e91b546c8cea041a3
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2025-08-07 17:21:12 +0100

    Merged pipeline extension into content project Renamed "solution" for content builder to "Content"

--- PlatformerContentBuilder/AnimatedModelProcessor/MeshAnimatedModelHelper.cs
+++ PlatformerContentBuilder/AnimatedModelProcessor/MeshAnimatedModelHelper.cs
@@ -2,14 +2,10 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.md', which is part of this source code package.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content.Pipeline;
 using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
 using Microsoft.Xna.Framework.Content.Pipeline.Processors;
-using Microsoft.Xna.Framework.Graphics;
 
 internal static class MeshAnimatedModelHelper
 {
